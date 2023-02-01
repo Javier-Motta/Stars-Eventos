1cf95c39c987e8f315503fd9767cd34bc4a53a8b
Javier Motta <javiier_motta@hotmail.com>
2023-02-01 15:32:28 -0500

    agregamos estructura base para el slide de otros servicios

--- main.js
+++ main.js
@@ -6,3 +6,27 @@ iconMobileMenu.addEventListener("click", toggleMobileMenu);
 function toggleMobileMenu(){
     lisElementMobileMenu.classList.toggle("inactive");
 }
+
+const recreationServiceList = [];
+
+recreationServiceList.push({
+    name: "ANIMACIÓN INFANTIL - A",
+    contens: "Recreación, Pintacaritas, Juegos dinámicos, Show de payaso (pedagógico o Familiar), Globoflexia, Concursos, Sonido",
+    price: 160000,
+    time: 3 + " horas",
+    image: s,
+});
+recreationServiceList.push({
+    name: "ANIMACIÓN INFANTIL - B",
+    contens: "Recreación, Pintacaritas, Juegos dinámicos, Show de payaso (pedagógico o Familiar), Globoflexia, Concursos",
+    price: 130000,
+    time: "2:30" + " horas",
+    image: s,
+});
+recreationServiceList.push({
+    name: "ANIMACIÓN INFANTIL - C",
+    contens: "Recreación, Pintacaritas, Juegos dinámicos, Recreador(a), Concursos",
+    price: 100000,
+    time: "2" + " horas",
+    image: s,
+})
