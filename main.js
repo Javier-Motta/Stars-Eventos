d856a2c02277e435947cc0ef878d696a3c1affdc
Javier Motta <javiier_motta@hotmail.com>
2023-02-01 15:48:24 -0500

    volviendo al evento click para el menu en mobile

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const iconMobileMenu = document.querySelector(".header-iconMobileMenu");
 const lisElementMobileMenu = document.querySelector (".header__lisElement-mobileMenu");
 
-iconMobileMenu.addEventListener("touchstart", toggleMobileMenu);
+iconMobileMenu.addEventListener("click", toggleMobileMenu);
 
 function toggleMobileMenu(){
     lisElementMobileMenu.classList.toggle("inactive");
