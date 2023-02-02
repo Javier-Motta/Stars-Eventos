7894adb706b98e56e6ed333770bc265c8e7c9373
Javier Motta <javiier_motta@hotmail.com>
2023-02-02 12:29:09 -0500

    Se agregó el boseto de un slider para describir los servicios, y es dinámico con js

--- main.js
+++ main.js
@@ -6,6 +6,30 @@ iconMobileMenu.addEventListener("click", toggleMobileMenu);
 function toggleMobileMenu(){
     lisElementMobileMenu.classList.toggle("inactive");
 }
+/* SLIDER SERVICE */
+(function(){
+    const sliders = [...document.querySelectorAll(".main__containerSlide-body")];
+    const arrowBefore = document.querySelector("#before");
+    const arrowAfter = document.querySelector("#after");
+    let value;
+
+    arrowBefore.addEventListener("click", ()=>changePosition(-1));
+    arrowAfter.addEventListener("click", ()=>changePosition(1));
+
+    function changePosition(change){
+        const currentElement =  Number(document.querySelector(".slider__body--show").dataset.id);
+
+        value = currentElement;
+        value+= change;
+
+        if (value === 0 || value == sliders.length+1) {
+            value = value === 0 ? sliders.length : 1;       
+        }
+        sliders[currentElement-1].classList.toggle("slider__body--show");
+        sliders[value-1].classList.toggle("slider__body--show");
+    }
+
+})()
 
 const recreationServiceList = [];
 
